2caa9ccc13f5198015d6ff98d01427d752abc4a7
Reece Smith <40872343+ReeceASmith@users.noreply.github.com>
2024-10-08 19:12:04 +0100

    Added paddle control and ball movement with top/bottom wall collision

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -15,6 +15,14 @@ const int gameWidth = 800;
 const int gameHeight = 600;
 const float paddleSpeed = 400.f;
 const float paddleOffsetWall = 10.f;
+const float paddleMaxY = gameHeight - paddleOffsetWall - paddleSize.y / 2.f;
+const float paddleMinY = paddleOffsetWall + paddleSize.y / 2.f;
+
+Vector2f ballVelocity;
+bool isPlayer1Serving = false;
+const float initialVelocityX = 200.f;
+const float initialVelocityY = 120.f;
+const float velocityMultiplier = 1.1f;
 
 CircleShape ball;
 RectangleShape paddles[2];
@@ -28,12 +36,13 @@ void Load() {
 	}
 	// Set size and origin of balls
 	ball.setRadius(ballRadius);
-	ball.setOrigin(Vector2f(ballRadius / 2.f, ballRadius / 2.f));
+	ball.setOrigin(sf::Vector2f(ballRadius / 2.f, ballRadius / 2.f));
 	// Reset paddle position
-	paddles[0].setPosition(Vector2f(paddleOffsetWall + paddleSize.x / 2.f, gameHeight / 2.f));
-	paddles[1].setPosition(Vector2f(gameWidth - paddleOffsetWall - paddleSize.x / 2.f, gameHeight / 2.f));
+	paddles[0].setPosition(sf::Vector2f(paddleOffsetWall + paddleSize.x / 2.f, gameHeight / 2.f));
+	paddles[1].setPosition(sf::Vector2f(gameWidth - paddleOffsetWall - paddleSize.x / 2.f, gameHeight / 2.f));
 	// Reset ball position
-	ball.setPosition(gameHeight / 2.f, gameWidth / 2.f);
+	ball.setPosition(gameWidth / 2.f, gameHeight / 2.f);
+	ballVelocity = { (isPlayer1Serving ? initialVelocityX : -initialVelocityX), initialVelocityY };
 }
 
 
@@ -41,6 +50,7 @@ void Update(RenderWindow& window) {
 	// Reset clock, recalculate deltatime
 	static Clock clock;
 	float dt = clock.restart().asSeconds();
+
 	// Check and consume events
 	Event event;
 	while (window.pollEvent(event)) {
@@ -56,12 +66,45 @@ void Update(RenderWindow& window) {
 	}
 
 	// Handle paddle movement
-	float direction = 0.f;
+	float direction1 = 0.f;
 	if (Keyboard::isKeyPressed(controls[0])) {
-		direction--;
+		direction1--;
 	}
 	if (Keyboard::isKeyPressed(controls[1])) {
-		direction++;
+		direction1++;
+	}
+	paddles[0].move(sf::Vector2f(0.f, direction1 * paddleSpeed * dt));
+	Vector2f paddlePosition[2];
+	paddlePosition[0] = paddles[0].getPosition();
+	paddles[0].setPosition(sf::Vector2f(paddlePosition[0].x, min(paddleMaxY, max(paddleMinY, paddlePosition[0].y))));
+
+
+	float direction2 = 0.f;
+	if (Keyboard::isKeyPressed(controls[2])) {
+		direction2--;
+	}
+	if (Keyboard::isKeyPressed(controls[3])) {
+		direction2++;
+	}
+	paddles[1].move(sf::Vector2f(0.f, direction2 * paddleSpeed * dt));
+	paddlePosition[1] = paddles[1].getPosition();
+	paddles[1].setPosition(sf::Vector2f(paddlePosition[1].x, min(paddleMaxY, max(paddleMinY, paddlePosition[1].y))));
+
+
+	// Move the ball
+	ball.move(ballVelocity * dt);
+
+	// Check ball collision
+	const float bx = ball.getPosition().x;
+	const float by = ball.getPosition().y;
+	if ((by + ballRadius) > gameHeight) { // Bottom wall
+		ballVelocity.x *= velocityMultiplier;
+		ballVelocity.y *= -velocityMultiplier;
+		ball.move(Vector2f(0.f, -10.f));
+	} else if ((by - ballRadius) < 0) { // Top wall
+		ballVelocity.x *= velocityMultiplier;
+		ballVelocity.y *= -velocityMultiplier;
+		ball.move(Vector2f(0.f, 10.f));
 	}
 	paddles[0].move(Vector2f(0.f, direction * paddleSpeed * dt));
 }
