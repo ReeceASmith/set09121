ed2187a085be6540f760cacc6c3ff2678647e246
Reece Smith <40872343+ReeceASmith@users.noreply.github.com>
2024-10-14 18:38:45 +0100

    Added ship implementation

---
+++ 2_invaders/ship.cpp
@@ -0,0 +1,18 @@
+#include "ship.h"
+#include "game.h"
+using namespace sf;
+using namespace std;
+
+Ship::Ship() {};
+
+Ship::Ship(IntRect ir) : Sprite() {
+	_sprite = ir;
+	setTexture(spritesheet);
+	setTextureRect(_sprite);
+};
+
+void Ship::Update(const float& dt) {}
+
+// Define the ship deconstructor
+// Although we set this to pure virtual, we still have to define it
+Ship::~Ship() = default;
