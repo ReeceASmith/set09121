39e21cc1a859b867e131d5926dbda1f39fca0dd3
Reece Smith <40872343+ReeceASmith@users.noreply.github.com>
2024-10-14 03:33:58 +0100

    Added setup sanity check code

--- 2_invaders/main.cpp
+++ 2_invaders/main.cpp
@@ -4,6 +4,11 @@ using namespace sf;
 using namespace std;
 
 
+const int gameWidth = 100;
+const int gameHeight = 100;
+
+
+
 
 void Load() {
 
@@ -14,18 +19,23 @@ void Reset() {
 
 }
 
+sf::CircleShape circle;
 
 void Update(RenderWindow& window) {
 	// Reset clock, recalculate deltatime
 	static Clock clock;
 	float dt = clock.restart().asSeconds();
 
+	circle = sf::CircleShape(10.f);
+	circle.setPosition(10, 10);
+	circle.setFillColor(sf::Color(255, 255, 0));
 }
 
 
 void Render(RenderWindow& window) {
 	// Draw everything
 	//window.draw(paddles[0]);
+	window.draw(circle);
 }
 
 
