1819f3e9d803d533ec77c77f38f691bdbbeb4d88
Reece Smith <40872343+ReeceASmith@users.noreply.github.com>
2024-10-15 07:36:38 +0100

    Fixed typo and added player pointer

--- 2_invaders/game.h
+++ 2_invaders/game.h
@@ -4,7 +4,8 @@
 constexpr uint16_t gameWidth = 800;
 constexpr uint16_t gameHeight = 600;
 constexpr uint16_t invaders_rows = 5;
-constexpr uint16_t invaders_columsn = 12;
+constexpr uint16_t invaders_columns = 12;
 
 extern sf::Texture spritesheet;
 extern std::vector<Ship*> ships;
+extern Player* player;
