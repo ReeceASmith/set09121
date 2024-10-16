2d518493fe87b1347b748f61754f24737452d12a
Reece Smith <40872343+ReeceASmith@users.noreply.github.com>
2024-10-16 15:08:15 +0100

    Fixed Entity::Update() to not use pointer to double dt

--- 3_tile_engine/Entity.cpp
+++ 3_tile_engine/Entity.cpp
@@ -5,7 +5,7 @@ using namespace sf;
 const Vector2f Entity::getPosition() { return _position; }
 void Entity::setPosition(const Vector2f& pos) { _position = pos; }
 void Entity::move(const Vector2f& pos) { _position += pos; }
-void Entity::Update(const double& dt) {
+void Entity::Update(const double dt) {
 	_shape->setPosition(_position);
 }
 Entity::Entity(unique_ptr<Shape> shp) : _shape(std::move(shp)) {}
